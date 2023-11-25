316de0aebfc0b0aab3fedbb2d64139a6c221c5f0
sandrabressan <bressan.sandra@yahoo.com.br>
2023-11-25 18:47:59 -0300

    propriedades lógicas

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+
+  title:string = ""
+
+  questions: any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
+  constructor (){}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
