0a0c25ae18de953da3d55d93fa57d625d8b775ed
sandrabressan <bressan.sandra@yahoo.com.br>
2023-11-25 19:44:30 -0300

    calculando o resultado

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from "../../../assets/data/quiz_questions.json"
 
 @Component({
   selector: 'app-quiz',
@@ -23,7 +24,49 @@ export class QuizComponent implements OnInit {
   constructor (){}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    if(quiz_questions){
+      this.finished = false
+      this.title = quiz_questions.title
+
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+    }
   }
+
+  playerChoose(value: string){
+    this.answers.push(value)
+    this.nextStep()
+  }
+
+  async nextStep(){
+    this.questionIndex += 1
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+      this.answerSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results]
+    }
+  }
+
+  async checkResult(answers: string[]){
+    const result = answers.reduce((previous, current, index, array) => {
+      if(
+        array.filter(item => item === previous).length >
+        array.filter(item => item === current).length
+        ){
+        return previous
+      }else{
+        return current
+      }
+    })
+    return result
+  }
+
+
 
 }
